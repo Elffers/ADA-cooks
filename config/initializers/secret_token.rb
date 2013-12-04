099024ab24ec57dfa933a54982304eb82603b288
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-03 17:26:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ADACooks::Application.config.secret_key_base = '8c7b185a35a2589f7621438b08694c920c813f8d3089250db55979048afe2a9c7d63770a28d103c4ca8fd3c52e17c4a7d8212dc972014922f5081839cd31b589'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ADACooks::Application.config.session_store :cookie_store, key: '_ADA-cooks_session'
