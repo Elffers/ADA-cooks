26956ef2d26531a2a2f78b4d4fc20d991c50bf69
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-12-06 15:05:49 -0800

    made search functionality work

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,21 @@ class Recipe < ActiveRecord::Base
   has_many :cookbooks, through: :cookbook_recipes
   has_many :gadget_recipes
   has_many :gadgets, through: :gadget_recipes
+
+  def self.by_ingredient_name(name)
+    includes(:ingredients).where(ingredients: {name: name})
+  end
+
+  def self.vegan
+    where(vegan: true)
+  end
+
+  def self.search(search_term)
+    if search_term
+      includes(:ingredients).where("(recipes.name) LIKE :s OR ingredients.name LIKE :s", s: "%#{search_term.downcase}%")
+    else
+      all
+    end
+  end
+
 end
